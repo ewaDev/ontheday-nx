0cab03192efb9f08bd7894d21e5b426d6a40c004
Ewa G <e.grabowiecka@outlook.com>
2023-03-23 14:20:40 +0100

    init

--- apps/internal-plugin-e2e/tests/internal-plugin.spec.ts
+++ apps/internal-plugin-e2e/tests/internal-plugin.spec.ts
@@ -4,12 +4,6 @@ import {
 } from '@nrwl/nx-plugin/testing';
 
 describe('internal-plugin e2e', () => {
-  // Setting up individual workspaces per
-  // test can cause e2e runs to take a long time.
-  // For this reason, we recommend each suite only
-  // consumes 1 workspace. The tests should each operate
-  // on a unique project in the workspace, such that they
-  // are not dependant on one another.
   beforeAll(() => {
     ensureNxProject('@bg-hoard/internal-plugin', 'dist/libs/internal-plugin');
   });
